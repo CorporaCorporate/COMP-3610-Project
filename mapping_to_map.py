dfa33a638b67b6510f197262a480769045e1951d
Enadiz76 <zidtimo@gmail.com>
2025-05-09 09:56:24 -0400

    clear

--- mapping_to_map.py
+++ mapping_to_map.py
@@ -11,11 +11,9 @@ data = pd.read_json(r"D:\UWI\Year 3\Sem 2\COMP3610-Big-Data\Project\Project Clon
 
 
 # Merge GeoDataFrame with predictions
-# Check your actual column name in the GeoJSON for constituency label
 # Adjust "constituency_name" to match
 gdf.columns  # Use this to check what the constituency column is named
 
-# Assuming it's called "Name", adjust as needed:
 merged = gdf.merge(data, left_on="Name", right_on="Constituency", how="left")
 
 # Map party names to colors
